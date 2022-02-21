c2a6a1783be4bbc3f2ec2c599aaf9fc332622370
panp.zhang001 <panp.zhang001@bkjk.com>
2022-02-21 21:25:42 +0800

    redux

--- React/my-react-app/src/demo/06ReduxDemo/store/react-redux/index.tsx
+++ React/my-react-app/src/demo/06ReduxDemo/store/react-redux/index.tsx
@@ -7,11 +7,36 @@ interface PropviderProps {
   store: any;
   children: React.ReactNode
 }
-// 
+
+// 类方式实现
+// export class Provider extends Component {
+//   static propTypes = {
+//       store: PropTypes.object,
+//       children: PropTypes.any
+//   }
+//   // 生产者消费者模式
+//   // 通过静态属性childCOntextTypes声明提供给子组件的Context对象的属性，并实现一个实例getChildContext方法，返回一个代表Context的纯对象(plain object)
+//   static childContextTypes = {
+//       store: PropTypes.object
+//   }
+//   // 上下文：使用Context,可以跨越组件进行数据传递
+//   getChildContext() {
+//       return {
+//           store: this.props.store
+//       }
+//   }
+//   render() {
+//       return (
+//           <div>{this.props.children}</div>
+//       )
+//   }
+// }
+// 函数实现
+const StoreContext = createContext(null)
 const Provider = (props: PropviderProps) => {
 
   const { store } = props;
-  const StoreContext = createContext(null)
+  
 
   return (
     <StoreContext.Provider value={store}>
@@ -21,41 +46,56 @@ const Provider = (props: PropviderProps) => {
 }
 
 interface State {
-  allState: any
+  allProps: any
+}
+interface Props {
+  store: any
 }
 
 // 高阶函数
+// 1. connect 接收两个参数 mapStateToProps, mapDispatchToProps,然后返回一个函数，这个返回的函数是高阶组件
+// 2. 这个高阶组件会接收一个组件作为参数，然后用新组件包装以后再返回
 const connect = (mapStateToProps: (states: any) => any, mapDispatchToProps: (dispatch: any) => any) => (OldComponent: any) => {
-  class NewComponent extends Component<{}, State> {
-
-    constructor(props: {}, state: State) {
+  class NewComponent extends Component<Props, State> {
+    constructor(props: Props, state: State) {
       super(props)
       this.state = {
-        allState: {}
+        allProps: {}
       }
     }
 
     componentDidMount() {
-      const allState = mapStateToProps(store.getState);
-      const allDispatch = mapDispatchToProps(store.dispatch);
+      const { store } = this.context
+      this._updateProps()
+      store.subscribe(() => this._updateProps())
+    }
+    _updateProps() {
+      const { store } = this.context;
+      const stateProps = mapStateToProps(store.getState);
+      const dispatchProps = mapDispatchToProps(store.dispatch);
       this.setState({
-        allState: Object.assign(this.state.allState, allState, allDispatch)
+        allProps: {
+          ...stateProps,
+          ...dispatchProps,
+          ...this.props
+        }
       })
     }
     render(): React.ReactNode {
       return (
         <>
-          <OldComponent {...this.state.allState} />
+          <OldComponent {...this.state.allProps} />
         </>
       )
     }
    
   }
+  NewComponent.contextType = StoreContext
 
   return NewComponent
 }
 
-export default {
+export {
   Provider,
   connect
 }
--- React/my-react-app/src/main.tsx
+++ React/my-react-app/src/main.tsx
@@ -5,16 +5,17 @@ import App from './App'
 import 'antd/dist/antd.css';
 
 // redux case
-// import ReduxDemoApp from './demo/06ReduxDemo'
-// import { store } from './demo/06ReduxDemo/store/createStore'
-// import { Provider } from 'react-redux'
+import ReduxDemoApp from './demo/06ReduxDemo'
+import { store } from './demo/06ReduxDemo/store/createStore'
+// import { Provider } from './demo/06ReduxDemo/store/react-redux'
+import {Provider } from 'react-redux'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
-    {/* <Provider store={store}>
+    {/* <App /> */}
+    <Provider store={store}>
       <ReduxDemoApp />
-    </Provider> */}
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 )
