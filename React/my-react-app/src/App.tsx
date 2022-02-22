750024cb08c2e3fa683b1ca9be5483d90184201b
panp.zhang001 <panp.zhang001@bkjk.com>
2022-02-22 10:02:33 +0800

    lifecycle

--- React/my-react-app/src/App.tsx
+++ React/my-react-app/src/App.tsx
@@ -1,26 +1,26 @@
-// import Counter from './demo/01class-lifecycle';
+import Counter from './demo/01class-lifecycle';
 // import Demo from './demo/cacheInterface';
 // import ClassContextDemo from './demo/02class-context';
 // import HighComp from './demo/03HighComp';
 // import HooksContext from './demo/04HooksContext'
 
 
-import Demo from './demo/07demo';
-import StateDemo from './demo/08setState'
+// import Demo from './demo/07demo';
+// import StateDemo from './demo/08setState'
 
 // import ClockDemo from './demo/clock'
 
 function App() {
   return (
     <div className="App">
-      {/* <Counter/> */}
+      <Counter/>
       {/* <Demo /> */}
       {/* <ClassContextDemo /> */}
       {/* <HighComp /> */}
       {/* <HooksContext /> */}
       {/* <Demo /> */}
       {/* <ClockDemo /> */}
-      <StateDemo />
+      {/* <StateDemo /> */}
     </div>
   )
 }
--- React/my-react-app/src/demo/01class-lifecycle.tsx
+++ React/my-react-app/src/demo/01class-lifecycle.tsx
@@ -8,11 +8,45 @@ interface Props {
 interface State {
   count: number
 }
+// 单组件生命周期
+// 父子组件生命周期
+class Sub extends React.Component<{count: number}> {
+  constructor(props: { count: number}) {
+    console.log('sub constructor')
+    super(props)
+  }
+  componentWillReceiveProps(nextProps: { count: number}) {
+    // 组件从父组件接收到新的 props 之前调用
+    console.log("sub componentWillReceiveProps", nextProps)
+  }
+  shouldComponentUpdate(nextProps: { count: number}) {
+    // 控制组件是否重新渲染
+    console.log("sub shouldComponentUpdate", nextProps)
+    return true
+  }
+  componentDidMount() {
+    console.log("sub componentDidMount")
+  }
+  componentDidUpdate() {
+    // 组件重新渲染并且把更改变更到真实的 DOM 以后调用
+    console.log("sub componentDidUpdate")
+  }
+  render() {
+    // 调用setState方法的时候走
+    console.log("sub render")
+    return (
+      <div>
+        sub: {this.props.count}
+      </div>
+    )
+  }
+}
 export default class Counter extends React.Component<Props, State> {
   static defaultProps = {
     count: 0
   }
   constructor(props: Props ) {
+    // state的初始化工作或者给自定义方法绑定this
     console.log('constructor')
     super(props)
   }
@@ -20,8 +54,10 @@ export default class Counter extends React.Component<Props, State> {
     count: this.props.count
   }
   componentWillMount() {
+    // 组件挂载开始之前， 也就是组件调用render方法之前调用。
     console.log('1. componentWillMount 组件将要挂载')
   }
+
 
   handleClick = () => {
     this.setState((prevState: any) => (
@@ -30,15 +66,25 @@ export default class Counter extends React.Component<Props, State> {
       }
     ))
   }
-
+  componentDidUpdate() {
+    // 组件重新渲染并且把更改变更到真实的 DOM 以后调用
+    console.log("parent componentDidUpdate")
+  }
   componentDidMount() {
+    // 组件挂载完成以后， 也就是DOM元素已经插入页面后调用
     console.log('3. componentDidMount 组件挂载完成')
+  }
+  componentWillUnmount() {
+    //  组件对应的DOM元素从页面中删除之前调用。
+    console.log('componentWillUnmount 组件卸载')
   }
   render() {
     console.log('2. render 渲染')
     return (
       <div>
         父计数器：{this.state.count}
+        <button onClick={this.handleClick}> plus 1</button>
+        <Sub count={this.state.count} />
       </div>
     )
   }
