ca0815e658089e662faf35868f1e877c7dfe640a
panp.zhang001 <panp.zhang001@bkjk.com>
2022-03-10 09:50:05 +0800

    react hooks需要注意的地方

--- React/my-react-app/src/demo/09Hooks/index.tsx
+++ React/my-react-app/src/demo/09Hooks/index.tsx
@@ -3,17 +3,41 @@ import Counter from './Sub'
 
 import UseRefDemo from './UseRefDemo'
 
+interface Props {
+  useInfo: {
+    name: string
+  }
+}
+
+function Child (props: Props) {
+  const { useInfo } = props
+  // render : 初始化 state
+  //! re-render: 只恢复初始化的 state 值，不会再重新设置新的值。(所以此时name的值不随userInfo.name变化的)
+  const [name, setName] = useState<string>(useInfo.name)
+  return (
+    <div>
+      child: { name }
+    </div>
+  )
+}
+
 const HooksDemo = () => {
   const [show, setShow] = useState<boolean>(false)
+  const [name, setName] = useState<string>('张三')
+  const useInfo = {
+    name
+  }
 
   return (
     <div>
       <h3>React Hooks from React 16.8 version</h3>
       <button onClick={() => setShow(false)}>show=false</button>
+      <button onClick={() => setName('李四')}>setName:{name}</button>
       {
         show && <Counter />
       }
       <UseRefDemo />
+      <Child useInfo={useInfo} />
     </div>
   )
 }
