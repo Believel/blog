263785f5f98aa3ccbe2b0702e410c40990a8bc99
panp.zhang001 <panp.zhang001@bkjk.com>
2022-04-13 16:54:13 +0800

    init miniprogram

---
+++ Engineering/miniProgram/miniprogram/envList.js
@@ -0,0 +1,6 @@
+const envList = [{"envId":"cloud1-8gjbe75v967dac71","alias":"cloud1"}]
+const isMac = true
+module.exports = {
+    envList,
+    isMac
+}
---
+++ Engineering/miniProgram/uploadCloudFunction.sh
@@ -0,0 +1,1 @@
+"/Applications/wechatwebdevtools.app/Contents/MacOS/cli" cloud functions deploy --e cloud1-8gjbe75v967dac71 --n quickstartFunctions --r --project "/Users/zpp/Desktop/blog/Engineering/miniProgram" --report_start --report
