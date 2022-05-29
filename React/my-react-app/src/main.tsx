fd7944d77a2dc4fc4ada3f92cc8feacf858738ee
tgd <zjrzpp0414@163.com>
2022-05-29 09:33:28 +0800

    react

--- React/my-react-app/src/main.tsx
+++ React/my-react-app/src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
 import 'antd/dist/antd.css';
-// import './source/index'
+import './source/index'
 
 // redux case
 import ReduxDemoApp from './demo/06ReduxDemo'
@@ -11,12 +11,12 @@ import { store } from './demo/06ReduxDemo/store/createStore'
 // import { Provider } from './demo/06ReduxDemo/store/react-redux'
 import {Provider } from 'react-redux'
 
-ReactDOM.render(
-  <React.StrictMode>
-    {/* <App /> */}
-    <Provider store={store}>
-      <ReduxDemoApp />
-    </Provider>
-  </React.StrictMode>,
-  document.getElementById('root')
-)
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//     <Provider store={store}>
+//       <ReduxDemoApp />
+//     </Provider>
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// )
